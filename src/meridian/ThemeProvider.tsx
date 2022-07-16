89feec0c24bcb7f339779d91aad6d4131ee7b3ed
Gabi <ig_999@yahoo.com>
2022-07-16 18:26:29 +0300

    Remove unused color from theme provider

--- src/meridian/ThemeProvider.tsx
+++ src/meridian/ThemeProvider.tsx
@@ -15,20 +15,6 @@ const ThemeProvider = ({ children }: Props) => {
     const theme: MantineThemeOverride = React.useMemo(
         () => ({
             colorScheme: settings.darkMode ? 'dark' : 'light',
-            colors: {
-                'ocean-blue': [
-                    '#7AD1DD',
-                    '#5FCCDB',
-                    '#44CADC',
-                    '#2AC9DE',
-                    '#1AC2D9',
-                    '#11B7CD',
-                    '#09ADC3',
-                    '#0E99AC',
-                    '#128797',
-                    '#147885',
-                ],
-            },
         }),
         [settings],
     );
