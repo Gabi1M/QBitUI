857c7c8bac8db4bb0df65bf7a0d1b83ba3ee518b
Gabi <ig_999@yahoo.com>
2022-07-16 12:24:31 +0300

    Fix package version

--- src/meridian/useAboutModal.tsx
+++ src/meridian/useAboutModal.tsx
@@ -13,7 +13,7 @@ const AboutModal = () => {
         <Box className={styles.classes.logo}>
             <Avatar size='xl' src={Icons.LOGO} />
             <Text mt='lg' size='lg'>
-                Version {import.meta.env.REACT_APP_VERSION}
+                Version {import.meta.env.VITE_VERSION}
             </Text>
             <Anchor<'a'> href='https://www.qbittorrent.org/' mt='lg' weight={700}>
                 QBitTorrent {versions.version} API {versions.apiVersion}
