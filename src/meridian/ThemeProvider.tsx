b5615af18a0f03dabfefe82929678a29bd438a2b
Gabi <ig_999@yahoo.com>
2022-07-16 18:27:32 +0300

    Remove unnecesarry theme ref

--- src/meridian/ThemeProvider.tsx
+++ src/meridian/ThemeProvider.tsx
@@ -7,9 +7,6 @@ interface Props {
     children: React.ReactNode;
 }
 
-let themeRef: MantineThemeOverride | null = null;
-export const getThemeRef = () => themeRef;
-
 const ThemeProvider = ({ children }: Props) => {
     const settings = useSelector(selectSettings);
     const theme: MantineThemeOverride = React.useMemo(
@@ -18,7 +15,6 @@ const ThemeProvider = ({ children }: Props) => {
         }),
         [settings],
     );
-    themeRef = theme;
     document.body.style.backgroundColor = settings.darkMode ? '#2C2E33' : '#FFFFFF';
 
     return <MantineProvider theme={theme}>{children}</MantineProvider>;
