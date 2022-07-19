270ec58ff57001e3938fa375d36e8b01dd544560
Gabi <ig_999@yahoo.com>
2022-07-19 21:17:46 +0300

    Extract snackbar types into separate type

--- src/meridian/snackbar/actions.ts
+++ src/meridian/snackbar/actions.ts
@@ -2,16 +2,18 @@ export enum SnackbarAction {
     SHOW_SNACKBAR = 'SHOW_SNACKBAR',
 }
 
+export type SnackbarVariant = 'default' | 'error' | 'info' | 'success' | 'warning';
+
 export interface ShowSnackbarAction {
     type: SnackbarAction;
     text: string;
-    variant: 'default' | 'error' | 'info' | 'success' | 'warning';
+    variant: SnackbarVariant;
     autoHideDuration?: number;
 }
 
 export const showSnackbarAction = (
     text: string,
-    variant: 'default' | 'error' | 'info' | 'success' | 'warning',
+    variant: SnackbarVariant,
     autoHideDuration?: number,
 ): ShowSnackbarAction => ({
     type: SnackbarAction.SHOW_SNACKBAR,
