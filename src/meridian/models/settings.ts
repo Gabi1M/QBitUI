8a872a2005863c0b89818b77e41f6321b6ad8b19
Gabi <gabriel.macovei@porsche-engineering.ro>
2022-08-05 20:41:58 +0300

    Add eslint rules and fix inconsistencies

--- src/meridian/models/settings.ts
+++ src/meridian/models/settings.ts
@@ -1,3 +1,4 @@
+/* eslint-disable-next-line no-restricted-imports */
 import { Language } from 'meridian/i18n/types';
 
 export interface Settings {
--- src/meridian/torrent/state/selectors.ts
+++ src/meridian/torrent/state/selectors.ts
@@ -1,3 +1,4 @@
+/* eslint-disable-next-line no-restricted-imports */
 import { GlobalState } from 'meridian/state/types';
 
 export const selectTorrents = (state: GlobalState) => state.torrentState.fetch.data;
